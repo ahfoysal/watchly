bb24802519032abf3c214ff60756e9e1c44785cb
hosan30 <50867996+hosan30@users.noreply.github.com>
2023-03-13 15:36:40 +0600

    dasd
    asda

--- src/PLYR/HLS.tsx
+++ src/PLYR/HLS.tsx
@@ -4,6 +4,7 @@ import {useEffect} from 'react';
 // import "plyr/dist/plyr.css";
 import "plyr-react/plyr.css"
 import './css.css'
+import { MdOutlineKeyboardBackspace } from 'react-icons/md';
 
 const VideoPlayer = ({src, sub}) => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -145,9 +146,10 @@ console.log(sub, src)
               
           
             </video>
-            <svg className="backicon back-icon" onClick={backbutton} focusable="false"  viewBox="0 0 24 24" aria-hidden="true" id="backbutton" >
+            {/* <svg className="backicon back-icon" onClick={backbutton} focusable="false"  viewBox="0 0 24 24" aria-hidden="true" id="backbutton" >
   <path d="M16.62 2.99c-.49-.49-1.28-.49-1.77 0L6.54 11.3c-.39.39-.39 1.02 0 1.41l8.31 8.31c.49.49 1.28.49 1.77 0s.49-1.28 0-1.77L9.38 12l7.25-7.25c.48-.48.48-1.28-.01-1.76z"></path>
-  </svg>
+  </svg> */}
+  <MdOutlineKeyboardBackspace className='backicon back-icon' onClick={backbutton} focusable={false}/>
             
         </div>
     )
